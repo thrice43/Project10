b476528ba0d85c64946488696b922a6bf5c53bf5
James <thrice43@gmail.com>
2011-04-13 02:05:10 -0400

    Template C++ file w/ Headers Commented out

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+//#include <fstream>
+//#include <cmath>
+//#include <string>
+//#include <>
+using namespace std;
+
+// Function Prototypes
+
+
+
+// Main function
+int main(){
+
+}
+
